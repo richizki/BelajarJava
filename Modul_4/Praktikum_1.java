f68898a0238d9f74b329ecf5a992ff0cb3cf6648
bangadam <bangadam.dev@gmail.com>
2018-09-25 22:43:16 +0700

    Tugas Praktikum No. 1

---
+++ Modul_4/Praktikum_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_4;
+
+/**
+ *
+ * @author bangadam
+ */
+public class Praktikum_1 {
+    public static void main(String[] args) {
+//        int  jml = 0;
+//        for (int i = 1; i <= 10; i++) {
+//            jml+=i;
+//        }
+//        System.out.println("Jumlah Semua bilangan : "+jml);
+        
+        System.out.println("===========================");
+        for (double jari = 1.0; jari <= 2.0; jari+=0.2) {
+            System.out.println("Radius ="+ jari+" Luas = "+jari*Math.PI*jari);
+        }
+    }
+}
