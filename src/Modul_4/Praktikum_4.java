52658ac2ff84cfd6219a125a9a54cba30f2544dd
LAB-PEMROGRAMAN.9 <LAB-PEMROGRAMAN.9@LAB-PEMROGRAMAN>
2018-09-26 07:39:21 +0700

    Tugas Praktikum No. 4

---
+++ src/Modul_4/Praktikum_4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LAB-PEMROGRAMAN.9
+ */
+public class Praktikum_4 {
+    public static void main(String[] args) {
+        int bilangan =0, batas = 5, hasil=0;
+         
+        for (int i = 0; i <batas; i++) {
+            bilangan = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            "Masukkan Bilangan sebanyak 5 kali, " +i+" kali"));
+            hasil+=bilangan;
+        }
+        
+        
+        System.out.println("Total jumlah Bilangan = "+ hasil);
+        
+        if (hasil%2 == 1) {
+            System.out.println("Bilangan termasuk ganjil");
+        } else {
+            System.out.println("Bilangan termasuk Genap");
+        }
+    }
+}
