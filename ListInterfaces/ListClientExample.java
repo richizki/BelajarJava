4a8d1bda25eb3ad865a1306150d8e6bf150c6bbf
bangadam <bangadam.dev@gmail.com>
2019-02-09 20:19:13 +0700

    update

--- ListInterfaces/ListClientExample.java
+++ ListInterfaces/ListClientExample.java
@@ -17,28 +17,21 @@ public class ListClientExample {
 	// deklarasi Interface List
 	private List list;
 
-	// COnstructor dari class ListClientExample
-//	public ListClientExample() {
-//		ArrayList<String> list = new ArrayList();
-//	}
+	// Constructor dari class ListClientExample
+	public ListClientExample() {
+	    ArrayList<String> list = new ArrayList();
+	}
         
-        // Memembuat method dengan mendapatkan data dari List
+        // Memmbuat method dengan mendapatkan data dari List
 	private List getList() {
-		return list;
+
+	    return list;
+
 	}
         
 	public static void main(String[] args) {
-//		ListClientExample ice = new ListClientExample();
-//		System.out.println(list);
-            LinkedList<String> turism = new LinkedList<String>();
-             //add elements to LinkedList
-             turism.add("JEMBATAN AMPERA");
-             turism.add("BENTENG KUTO BESAK");
-             turism.add("KAMBANG IWAK");
-             turism.add("PUNTI KAYU");
-             turism.add("AIR TERJUN BEDEGUM");
-             turism.add("SUNGAI MUSI");
-             
-             System.out.println(turism);
+		ListClientExample ice = new ListClientExample();
+		List list = ice.getList();
+		System.out.println(list);
 	}
 }
